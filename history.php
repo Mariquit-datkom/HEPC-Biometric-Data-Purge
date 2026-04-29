962acc1c541180b12a1ef124c29c34be09c95a92
Lance Mariquit <lancemariquit.1214@gmail.com>
2026-04-29 13:28:51 +0800

    new: ui for history tab

--- history.php
+++ history.php
@@ -1,5 +1,14 @@
 <?php 
 require_once 'x-head.php';
+require_once 'userHeartbeatChecker.php';
+
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Pragma: no-cache");
+
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php?reason=invalid_session");
+    exit();
+}
 
 $currentPage = basename($_SERVER['PHP_SELF']);
 ?>
@@ -10,11 +19,30 @@ $currentPage = basename($_SERVER['PHP_SELF']);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/global.css">
+    <link rel="stylesheet" href="css/history.css">
     <title>History - Biometric Data Purge</title>
 </head>
 <body>
     <?php include 'header.php' ?>
     <?php include 'navPanel.php' ?>
     
+    <div class="history-main-container">
+        <div class="history-content">
+            <div class="device-list-container">
+                <?php foreach ($devices as $index => $item): ?>
+                <div class="device"
+                data-name="<?php echo trim(strtolower($item['name'])) ?>"
+                data-ip="<?php echo trim(strtolower($item['ip'])) ?>">
+                    <span class="device-main"><?php echo $item['name'] ?></span>
+                </div>
+                <?php endforeach; ?>
+            </div>
+            <div class="date-list-container">
+                
+            </div>
+        </div>
+    </div>
+    
+    <script src="js/userHeartbeat.js"></script>
 </body>
 </html>
