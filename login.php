2c004d3b4358a73ec3702a33c71eb315acafdf60
Lance Mariquit <lancemariquit.1214@gmail.com>
2026-04-29 10:09:50 +0800

    Added heartbeat checker function in login page

--- login.php
+++ login.php
@@ -1,6 +1,9 @@
 <?php
 require_once 'x-head.php';
+require_once 'userHeartbeatChecker.php';
 
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Pragma: no-cache");
 ?>
 
 <!DOCTYPE html>
