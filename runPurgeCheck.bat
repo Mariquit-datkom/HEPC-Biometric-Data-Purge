caf7984447d0783aed18227600c893af0795b93c
Lance Mariquit <lancemariquit.1214@gmail.com>
2026-05-05 09:15:56 +0800

    new: added bat action file for task scheduling

---
+++ runPurgeCheck.bat
@@ -0,0 +1,13 @@
+@echo off
+set "SCRIPT_PATH=%~dp0"
+cd /d "%SCRIPT_PATH%"
+
+where php >nul 2>&1
+if %errorlevel% neq 0 (
+    echo PHP not found in PATH. Please install XAMPP or add PHP to your Environment Variables.
+    pause
+    exit /b
+)
+
+php notify.php
+exit
